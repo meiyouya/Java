3d8330885eb6253082eac7711df346c21ac9c196
meiyouya <837184598@qq.com>
2019-10-21 23:37:24 +0800

    JsonParseException and read character

---
+++ json/src/main/java/com/lawliet/json/exceprion/JsonParseException.java
@@ -0,0 +1,11 @@
+package com.lawliet.json.exceprion;
+
+/**
+ * @author lawliet.L
+ */
+public class JsonParseException extends RuntimeException{
+
+    public JsonParseException(String message) {
+        super(message);
+    }
+}
