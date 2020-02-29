48bdf11176faca39a9c0e43ee6209993b73d2d06
meiyouya <837184598@qq.com>
2020-03-01 00:15:53 +0800

    Tesseract

---
+++ basic/src/com/lawliet/ocr/Tesseract.java
@@ -0,0 +1,14 @@
+package com.lawliet.ocr;
+
+import java.io.File;
+
+/**
+ * @author lawliet.L
+ */
+public class Tesseract {
+
+    public String distinguishDigit(File image) {
+        File result = new File(image.getParentFile(), "result");
+
+    }
+}
