44dc853d99fb5a2ecbc66834764202246761f575
lawliet-zhang <1259420947@qq.com>
2020-06-06 19:45:44 +0800

    char sequence compare

--- basic/src/com/lawliet/string/StringCompare.java
+++ basic/src/com/lawliet/string/StringCompare.java
@@ -1,14 +1,31 @@
-package com.zql.string;
+package com.lawliet.string;
 
 public class StringCompare {
 
     public static void main(String[] args) {
-        String str = "Hello World";
+        /*String str = "Hello World";
         String anotherStr = "hello world";
         Object objStr = str;
 
         System.out.println(str.compareTo(anotherStr));
         System.out.println(str.compareToIgnoreCase(anotherStr));
-        System.out.println(str.compareTo(objStr.toString()));
+        System.out.println(str.compareTo(objStr.toString()));*/
+
+        System.out.println(compareString("abc1", "abc"));
+    }
+
+    private static boolean compareString(String a, String b) {
+        int aLength = a.length();
+        int bLength = b.length();
+        if (aLength != bLength) {
+            return false;
+        }
+        int len = Math.max(aLength, bLength);
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
