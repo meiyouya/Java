012bc2d83354f37033d7b5d5cffb9a73a9d696c6
meiyouya <31955743+meiyouya@users.noreply.github.com>
2020-06-16 23:32:03 +0800

    stream

---
+++ basic/src/com/lawliet/stream/StreamTest.java
@@ -0,0 +1,17 @@
+package com.lawliet.stream;
+
+/**
+ * @author lawliet.L
+ */
+public class StreamTest {
+
+    public static void main(String[] args) {
+//        Random random = new Random();
+//        random.ints().limit(10).forEach(System.out::println);
+
+//        List<Integer> list = Arrays.asList(1, 2, 2, 4, 5, 6);
+//        List<Integer> collect = list.stream().map(num -> num * num).collect(Collectors.toList());     // 输出 [1, 4, 4, 16, 25, 36]
+//        List<Integer> collect = list.stream().map(num -> num * num).distinct().collect(Collectors.toList());    // 输出 [1, 4, 16, 25, 36]
+//        System.out.println(collect);
+    }
+}
