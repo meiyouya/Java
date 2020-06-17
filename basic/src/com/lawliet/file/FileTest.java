ae4c06a33b44af35d01f1be48f92022b67fbc7fa
lawliet-zhang <1259420947@qq.com>
2020-06-17 17:36:54 +0800

    stream and file

--- basic/src/com/lawliet/file/FileTest.java
+++ basic/src/com/lawliet/file/FileTest.java
@@ -1,7 +1,6 @@
-package com.zql.file;
+package com.lawliet.file;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author lawliet.L
@@ -9,7 +8,7 @@ import java.io.IOException;
 public class FileTest {
 
     public static void main(String[] args) {
-        File file = new File("test.txt");
+        /*File file = new File("test.txt");
         if (file.exists()) {
             System.out.println("文件已存在");
             System.out.println("文件名称：" + file.getName());
@@ -23,6 +22,43 @@ public class FileTest {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }*/
+
+        try {
+            cut("F:\\zql\\idea_work\\Java\\basic\\src\\com\\lawliet\\file\\test.txt", 20);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * 切割文件
+     * @param filePath 文件全路径
+     * @param size 切割后的每个文件的大小
+     * @throws FileNotFoundException 文件找不到
+     */
+    private static void cut(String filePath, int size) throws IOException {
+        File file = new File(filePath);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        long fileLength = file.length();
+        int count = (int) (fileLength % size == 0 ? fileLength / size : (fileLength / size) + 1);
+
+        byte[] bytes = new byte[(int) fileLength];
+        fileInputStream.read(bytes);
+        fileInputStream.close();
+
+        for (int i = 1; i <= count; i++) {
+            String cutFileName = file.getParent() + File.separator + "cut" + i + ".txt";
+            FileOutputStream fileOutputStream = new FileOutputStream(cutFileName);
+            if (i == count) {
+                // 最后一个文件单独处理
+                fileOutputStream.write(bytes, size * (i - 1), (int) (fileLength - size * (i - 1)));
+            } else {
+                fileOutputStream.write(bytes, size * (i - 1), size);
+            }
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        }
+
     }
 }
--- basic/src/com/lawliet/stream/StreamTest.java
+++ basic/src/com/lawliet/stream/StreamTest.java
@@ -13,5 +13,27 @@ public class StreamTest {
 //        List<Integer> collect = list.stream().map(num -> num * num).collect(Collectors.toList());     // 输出 [1, 4, 4, 16, 25, 36]
 //        List<Integer> collect = list.stream().map(num -> num * num).distinct().collect(Collectors.toList());    // 输出 [1, 4, 16, 25, 36]
 //        System.out.println(collect);
+
+//        List<Integer> list = Arrays.asList(1, 2, 2, 4, 5, 6);
+//        list.stream().filter(num -> num > 3).forEach(System.out::println);
+
+//        List<Integer> list = Arrays.asList(1, 2, 2, 4, 5, 6);
+//        list.stream().filter(num -> num > 3).limit(2).forEach(System.out::println);
+
+//        List<Integer> list = Arrays.asList(1, 2, 2, 4, 5, 6);
+//        list.stream().filter(num -> num > 3).collect(Collectors.toList()).forEach(System.out::println);
+
+//        List<Integer> list = Arrays.asList(1, 2, 2, 4, 5, 6);
+//        IntSummaryStatistics statistics = list.stream().mapToInt((x) -> x).summaryStatistics();
+//        System.out.println(statistics.getAverage());
+//        System.out.println(statistics.getMax());
+//        System.out.println(statistics.getCount());
+//        System.out.println(statistics.getMin());
+//        System.out.println(statistics.getSum());
+
+//        List<String> list = Arrays.asList("hello world", "hello java", "hello every one");
+//        list.stream().map(v -> v.split(" ")).peek(System.out::println).count();
+//        list.stream().flatMap(v -> Arrays.stream(v.split(" "))).peek(System.out::println).count();
+
     }
 }
