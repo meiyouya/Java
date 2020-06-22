d125f34dfafe472857ed4f2f3e850815388b4abe
lawliet-zhang <1259420947@qq.com>
2020-06-22 16:50:14 +0800

    ByteCodeSample

---
+++ jvm/src/com/zql/stack/ByteCodeSample.java
@@ -0,0 +1,13 @@
+package com.zql.stack;
+
+/**
+ * @author dell
+ */
+public class ByteCodeSample {
+
+    public static int add(int a, int b) {
+        int c = 0;
+        c = a + b;
+        return c;
+    }
+}
