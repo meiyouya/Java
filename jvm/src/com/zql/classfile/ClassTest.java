73924df8ceedb5ca4c113f081085eab135a4625a
meiyouya <837184598@qq.com>
2020-10-07 13:41:26 +0800

    class file

---
+++ jvm/src/com/zql/classfile/ClassTest.java
@@ -0,0 +1,14 @@
+package com.zql.classfile;
+
+/**
+ * 测试class文件的内容与结构
+ * @author lawliet.L
+ */
+public class ClassTest {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
